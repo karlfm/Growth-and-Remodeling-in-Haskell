f73030b008d3dc95a413a8923979cce5cb7215ae
Karl Munthe <karlfredrik@simula.no>
2025-02-21 16:34:57 +0100

    cleaned up code

--- cylinder_growth.hs
+++ cylinder_growth.hs
@@ -15,103 +15,70 @@ updateR :: State
         -> Double   -- inner radius in new configuration          
         -> Double   -- point in reference (previous) configuration
         -> Double   -- r
-updateR state a oldR =
-    let gammaR' = gammaR state
-        gammaTheta' = gammaTheta state
-        innerBoundary' = innerBoundary state
-    in  sqrt(a ** 2 + gammaR' * gammaTheta' * (oldR ** 2 - innerBoundary' ** 2))
+updateR st a oldR = sqrt(a ** 2 + (gammaR st) * (gammaTheta st) * (oldR ** 2 - (innerBoundary st) ** 2))
 
 -- αr^(i+1) = αr^i R γθ / r
 updateAlphaR :: State 
              -> Double  -- inner radius in new configuration          
              -> Double  -- point in reference (previous) configuration
              -> Double  -- αr^(i+1)
-updateAlphaR state a oldR = 
-    let gammaTheta' = gammaTheta state
-        alphaR' = alphaR state
-        r' = updateR state a oldR
-    in  alphaR' * oldR * gammaTheta' / r'
+updateAlphaR st a oldR = 
+    let r = updateR st a oldR
+    in  (alphaR st) * oldR * (gammaTheta st) / r
 
 -- αθ^(i+1) = αθ^i r / (R γθ)
 updateAlphaTheta :: State 
                  -> Double  -- inner radius in new configuration          
                  -> Double  -- point in reference (previous) configuration
                  -> Double  -- αθ^(i+1)
-updateAlphaTheta state a oldR = 
-    let gammaTheta' = gammaTheta state
-        alphaTheta' = alphaTheta state
-        r' = updateR state a oldR
-    in  alphaTheta' * r' / (oldR * gammaTheta')
+updateAlphaTheta st a oldR = 
+    let r = updateR st a oldR
+    in  (alphaTheta st) * r / (oldR * (gammaTheta st))
 
 -- P = μγr γθ ∫_A^B s r^2 (α_θ)^2 − (α_r)^2 ds
 calculateP :: State 
            -> Double    -- inner radius in new configuration
            -> Double    -- P
-calculateP state a = 
-    let mu' = mu state
-        gammaR' = gammaR state
-        gammaTheta' = gammaTheta state
-        alphaTheta' = alphaTheta state
-        alphaR' = alphaR state
-        innerBoundary' = innerBoundary state
-        outerBoundary' = outerBoundary state
-
-        r = (\s -> sqrt(a ** 2 + gammaR' * gammaTheta' * (s ** 2 - innerBoundary' ** 2)))
-        alphaR_ = (\s -> alphaR' * s * gammaTheta' / (r s))
-        alphaTheta_ = (\s -> alphaTheta' * (r s) / (s * gammaTheta'))
+calculateP st a = 
+    let r = updateR st a 
+        alphaR_ = updateAlphaR st a  
+        alphaTheta_ = updateAlphaTheta st a 
 
         integrand = (\s -> s/(r s)**2 * ((alphaTheta_ s) ** 2 - (alphaR_ s) ** 2))
-        integral = quadrature64 integrand innerBoundary' outerBoundary'
-    in mu' * gammaR' * gammaTheta' * integral
+        integral = quadrature64 integrand (innerBoundary st) (outerBoundary st)
+
+    in (mu st) * (gammaR st) * (gammaTheta st) * integral
 
 findInnerBoundary :: State -> Double
-findInnerBoundary state = 
-    let innerBoundary' = innerBoundary state
-        outerBoundary' = outerBoundary state
-    in  secant 1e-14 (\a -> calculateP state a) (innerBoundary', outerBoundary')  -- here i am assuming the new inner boundary is between the old boundaries
+findInnerBoundary st = secant 1e-14 (\a -> calculateP st a) ((innerBoundary st), (outerBoundary st))  -- here i am assuming the new inner boundary is between the old boundaries
 
 -- σrr = μγr γθ ∫_A^R s r^2 (α_θ)^2 − (α_r)^2 ds − P
 sigmaRR :: State 
         -> Double   -- inner radius in new configuration          
         -> Double   -- point in reference (previous) configuration
         -> Double   -- σrr
-sigmaRR state a oldR = 
-    let mu' = mu state
-        gammaR' = gammaR state
-        gammaTheta' = gammaTheta state
-        alphaTheta' = alphaTheta state
-        alphaR' = alphaR state
-        innerBoundary' = innerBoundary state
-
-        r = (\s -> sqrt(a ** 2 + gammaR' * gammaTheta' * (s ** 2 - innerBoundary' ** 2)))
-        alphaR_ = (\s -> alphaR' * s * gammaTheta' / (r s))
-        alphaTheta_ = (\s -> alphaTheta' * (r s) / (s * gammaTheta'))
-        p = calculateP state a
+sigmaRR st a oldR = 
+    let r = updateR st a 
+        alphaR_ = updateAlphaR st a  
+        alphaTheta_ = updateAlphaTheta st a 
+        p = calculateP st a
 
         integrand = (\s -> s / (r s)**2 * ((alphaTheta_ s) ** 2 - (alphaR_ s) ** 2))
-        integral = quadrature64 integrand innerBoundary' oldR
-    in  mu' * gammaR' * gammaTheta' * integral - p
+        integral = quadrature64 integrand (innerBoundary st) oldR
+    in  (mu st) * (gammaR st) * (gammaTheta st) * integral - p
 
 -- σθθ = μγr γθ ∫_A^R s r^2 (α_θ)^2 − (α_r)^2 ds + σrr
 sigmaTT :: State 
         -> Double   -- inner radius in new configuration          
         -> Double   -- point in reference (previous) configuration 
         -> Double   -- σθθ
-sigmaTT state a oldR = 
-    let mu' = mu state
-        gammaR' = gammaR state
-        gammaTheta' = gammaTheta state
-        alphaTheta' = alphaTheta state
-        alphaR' = alphaR state
-        innerBoundary' = innerBoundary state
-        outerBoundary' = outerBoundary state
-
-        r = (\s -> sqrt(a ** 2 + gammaR' * gammaTheta' * (s ** 2 - innerBoundary' ** 2)))
-        alphaR_ = (\s -> alphaR' * s * gammaTheta' / (r s))
-        alphaTheta_ = (\s -> alphaTheta' * (r s) / (s * gammaTheta'))
+sigmaTT st a oldR = 
+    let r = updateR st a 
+        alphaR_ = updateAlphaR st a  
+        alphaTheta_ = updateAlphaTheta st a 
 
         dSigmaRR = (\s -> s / (r s)**2 * ((alphaTheta_ s) ** 2 - (alphaR_ s) ** 2))
-    in  mu' * gammaR' * gammaTheta' * (dSigmaRR oldR) * (r oldR)**2 / (oldR * gammaR' * gammaTheta') + (sigmaRR state a oldR)
+    in  (mu st) * (gammaR st) * (gammaTheta st) * (dSigmaRR oldR) * (r oldR)**2 / (oldR * (gammaR st) * (gammaTheta st)) + (sigmaRR st a oldR)
 
 main :: IO ()
 main = do
